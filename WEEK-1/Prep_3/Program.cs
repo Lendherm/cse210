cf99e4a6b85800c359b0730bb601a9ee7bf1b8c5
Lendherm <dluisemilionathanael@gmail.com>
2024-01-11 13:50:02 -0600

    finished

--- WEEK-1/Prep_3/Program.cs
+++ WEEK-1/Prep_3/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Task_3
+{
+    static void Main()
+    {
+        string playAgain;
+        do
+        {
+            Console.Write("What is the magic number? ");
+            int magicNumber = int.Parse(Console.ReadLine());
+            int numberOfGuesses = 0;
+
+            int guess;
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                numberOfGuesses++;
+
+                if (guess < magicNumber)
+                    Console.WriteLine("Higher");
+                else if (guess > magicNumber)
+                    Console.WriteLine("Lower");
+                else
+                    Console.WriteLine($"You guessed it in {numberOfGuesses} guesses!");
+
+            } while (guess != magicNumber);
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+        } while (playAgain == "yes");
+    }
+}
