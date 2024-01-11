f1e61dd56c9d9ff456c425cf6c50ca9e8f21a803
Lendherm <dluisemilionathanael@gmail.com>
2024-01-11 12:57:17 -0600

    first commit

---
+++ WEEK-1/Prep_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Task_1
+{
+    static void Main()
+    {
+        // Prompt the user for their first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Prompt the user for their last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Display the formatted output
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+    }
+}
