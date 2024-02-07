18b9e5aec9fb07240dda889a03e74167108103b6
Lendherm <dluisemilionathanael@gmail.com>
2024-02-07 10:27:30 -0600

    Learning05

---
+++ WEEK-5/Learning05/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Square square = new Square("Red", 5);
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Circle circle = new Circle("Green", 3);
+
+        List<Shape> shapes = new List<Shape> { square, rectangle, circle };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.Color}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
+    }
+}
